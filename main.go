0ed02dedbd54675d733bd32d7615f74bd4bcd797
alekseiefanov <ligawowa@gmail.com>
2026-02-18 17:03:43 +0500

    Changes

--- main.go
+++ main.go
@@ -1,75 +1,168 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"os"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
-	"log"
-	"net/http"
-	"strconv"
+	"github.com/redis/go-redis/v9"
 )
+
+type whoamiResponse struct {
+	IP string `json:"ip"`
+}
+
+type visitResponse struct {
+	IP     string `json:"ip"`
+	Visits int64  `json:"visits"`
+}
+
+type statsResponse struct {
+	UniqueIPs int64 `json:"unique_ips"`
+}
+
+type healthResponse struct {
+	Status string `json:"status"`
+}
+
+type handler struct {
+	store Store
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Println("no .env file found, using system env")
+		slog.Info("No .env file found, using system env")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
 
 	r := chi.NewRouter()
 
 	r.Use(ipMiddleware)
 
-	store := NewMemoryStore()
+	store := NewStore()
 
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"health":"ok"}`))
-	})
+	h := &handler{store: store}
 
-	r.Get("/whoami", func(w http.ResponseWriter, r *http.Request) {
-		ip, ok := GetIP(r.Context())
-		if !ok {
-			http.Error(w, "ip not found", http.StatusInternalServerError)
-			return
+	r.Get("/health", handleHealth)
+
+	r.Get("/whoami", handleWhoami)
+
+	r.Get("/visit", h.handleVisits)
+
+	r.Get("/stats", h.handleStats)
+
+	if err = http.ListenAndServe(":"+port, r); err != nil {
+		slog.Error("Error starting server:", "err", err)
+		os.Exit(1)
+	}
+}
+
+func NewStore() Store {
+	storeType := os.Getenv("STORE_TYPE")
+	if storeType == "" {
+		storeType = "memory"
+	}
+
+	switch storeType {
+	case "redis":
+		addr := os.Getenv("REDIS_ADDR")
+		if addr == "" {
+			addr = "localhost:6379"
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"ip":"`+ip+`"}`))
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     addr,
+			Password: os.Getenv("REDIS_PASSWORD"),
+			DB:       0,
+		})
 
-	})
-
-	r.Get("/visit", func(w http.ResponseWriter, r *http.Request) {
-		ip, ok := GetIP(r.Context())
-		if !ok {
-			http.Error(w, "ip not found", http.StatusInternalServerError)
-			return
+		if err := rdb.Ping(context.Background()).Err(); err != nil {
+			slog.Error("redis connection failed: %v", "err", err)
 		}
 
-		visits, err := store.Inc(ip)
-		if err != nil {
-			http.Error(w, "error update ip visits", http.StatusInternalServerError)
-			return
-		}
-		n := strconv.Itoa(visits)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"ip":"` + ip + `", "visits":` + n + `}`))
-	})
+		return NewRedisStore(rdb)
 
-	r.Get("/stats", func(w http.ResponseWriter, r *http.Request) {
-		count, err := store.UniqueCount()
-		if err != nil {
-			http.Error(w, "error upload stats", http.StatusInternalServerError)
-			return
-		}
-		n := strconv.Itoa(count)
+	default:
+		return NewMemoryStore()
+	}
+}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"unique_ips":` + n + `}`))
-	})
+func handleHealth(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(healthResponse{Status: "healthy"})
+	if err != nil {
+		slog.Error("Error encoding health response", "err", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+}
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+func handleWhoami(w http.ResponseWriter, r *http.Request) {
+	ip, ok := GetIP(r.Context())
+	if !ok {
+		slog.Info("IP not found")
+		http.Error(w, "ip not found", http.StatusInternalServerError)
+		return
+	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(whoamiResponse{IP: ip})
+	if err != nil {
+		slog.Error("Error encoding ip response", "err", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *handler) handleVisits(w http.ResponseWriter, r *http.Request) {
+	ip, ok := GetIP(r.Context())
+	if !ok {
+		slog.Info("IP not found")
+		http.Error(w, "ip not found", http.StatusInternalServerError)
+		return
+	}
+
+	visits, err := h.store.Inc(ip)
+	if err != nil {
+		slog.Error("Error update ip visits:", "err", err)
+		http.Error(w, "error update ip visits", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(visitResponse{IP: ip, Visits: visits})
+	if err != nil {
+		slog.Error("Error encoding visits response:", "err", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *handler) handleStats(w http.ResponseWriter, _ *http.Request) {
+	count, err := h.store.UniqueCount()
+	if err != nil {
+		slog.Error("Error upload stats", "err", err)
+		http.Error(w, "error upload stats", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(statsResponse{UniqueIPs: count})
+	if err != nil {
+		slog.Error("Error encoding stats response", "err", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
 }
--- store.go
+++ store.go
@@ -1,34 +1,59 @@
 package main
 
 import (
+	"context"
 	"sync"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type Store interface {
-	Inc(ip string) (int, error)
-	UniqueCount() (int, error)
+	Inc(ip string) (int64, error)
+	UniqueCount() (int64, error)
+}
+
+type RedisStore struct {
+	client *redis.Client
 }
 
 type MemoryStore struct {
-	reg map[string]int
+	reg map[string]int64
 	mu  sync.RWMutex
+}
+
+func NewRedisStore(client *redis.Client) *RedisStore {
+	return &RedisStore{
+		client: client,
+	}
+}
+
+func (c *RedisStore) Inc(ip string) (int64, error) {
+	newValue, err := c.client.HIncrBy(context.TODO(), "ip", ip, 1).Result()
+	if err != nil {
+		return 0, err
+	}
+	return newValue, nil
+}
+
+func (c *RedisStore) UniqueCount() (int64, error) {
+	return c.client.HLen(context.TODO(), "ip").Result()
 }
 
 func NewMemoryStore() *MemoryStore {
 	return &MemoryStore{
-		reg: make(map[string]int),
+		reg: make(map[string]int64),
 	}
 }
 
-func (ms *MemoryStore) Inc(ip string) (int, error) {
+func (ms *MemoryStore) Inc(ip string) (int64, error) {
 	ms.mu.Lock()
 	defer ms.mu.Unlock()
 	ms.reg[ip]++
 	return ms.reg[ip], nil
 }
 
-func (ms *MemoryStore) UniqueCount() (int, error) {
+func (ms *MemoryStore) UniqueCount() (int64, error) {
 	ms.mu.RLock()
 	defer ms.mu.RUnlock()
-	return len(ms.reg), nil
+	return int64(len(ms.reg)), nil
 }
